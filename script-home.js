b01c8894b77f2fa61b731e320ae735f61efab816
spiano <lafleurpiano@gmail.com>
2019-09-14 12:53:58 -0500

    added a modal to my story button with dummy text for now

--- script-home.js
+++ script-home.js
@@ -1,1 +1,14 @@
 console.log('welcome to the rabbit hole');
+const openBtn = document.getElementById('openModal');
+const modal = document.getElementById('modal');
+const close = document.getElementById('close');
+
+const openModal = () => {
+    modal.style.display = 'block';
+};
+
+openBtn.addEventListener('click', openModal);
+const closeModal = () => {
+    modal.style.display = 'none'
+};
+close.addEventListener('click', closeModal);
