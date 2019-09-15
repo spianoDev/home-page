7653892b58f518ce7cf5464740b42e7bc2213c7f
spiano <lafleurpiano@gmail.com>
2019-09-15 16:28:34 -0500

    added html pages for the other apps I have made

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # home-page
-The initial design is meant to look like code. I also wanted a color scheme that was gentle to the eyes.
+The initial design is meant to look like code. 
+I also wanted a color scheme that was gentle to the eyes.
--- script-home.js
+++ script-home.js
@@ -12,3 +12,20 @@ const closeModal = () => {
     modal.style.display = 'none'
 };
 close.addEventListener('click', closeModal);
+
+const miniGamesButton = document.getElementById('mini-games');
+const practicalAppsButton = document.getElementById('practical-apps');
+const funStuffButton = document.getElementById('fun-stuff');
+
+function newPage(page) {
+    page.addEventListener('click', function (e) {
+        if (page === miniGamesButton) {
+            window.location.href = "http://spiano.dev/home-page/mini-games.html";
+        } else if (page === practicalAppsButton) {
+            window.location.href = "http://spiano.dev/home-page/practical-apps.html";
+        } else if (page === funStuffButton) {
+            window.location.href = "http://spiano.dev/home-page/fun-stuff.html";
+        }
+
+    })
+}
