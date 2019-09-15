fc730e66d70c9e92bd38e0c04b8c88779f439583
spiano <lafleurpiano@gmail.com>
2019-09-15 17:02:52 -0500

    added the rest of the buttons and a hover to check for functionality

--- apps-script.js
+++ apps-script.js
@@ -1,1 +1,8 @@
 console.log('welcome to the rabbit hole');
+const atmButton = document.getElementById('atm');
+
+atmButton.addEventListener('click', function (e) {
+    if (e.target) {
+        window.location.href = "http://spiano.dev/atm";
+    }
+});
--- fun-script.js
+++ fun-script.js
@@ -1,1 +1,8 @@
 console.log('welcome to the rabbit hole');
+const pixButton = document.getElementById('pixart');
+
+pixButton.addEventListener('click', function (e) {
+    if (e.target) {
+        window.location.href = "http://spiano.dev/pixart";
+    }
+});
