ed79d00dfa8b93240d4dbd24182325c33d990e92
spiano <lafleurpiano@gmail.com>
2019-09-20 12:13:43 -0500

    added simon to the mini-games

--- games-script.js
+++ games-script.js
@@ -2,6 +2,7 @@ console.log('welcome to the rabbit hole');
 
 const landscaperButton = document.getElementById('landscaper');
 const spaceButton = document.getElementById('space-battle');
+const simonButton = document.getElementById('simon');
 
 landscaperButton.addEventListener('click', function (e) {
     if (e.target) {
@@ -12,5 +13,11 @@ landscaperButton.addEventListener('click', function (e) {
 spaceButton.addEventListener('click', function (e) {
     if (e.target) {
         window.location.href = "http://spiano.dev/spacebattle";
+    }
+});
+
+simonButton.addEventListener('click', function (e) {
+    if (e.target) {
+        window.location.href = "http://spiano.dev/simon";
     }
 });
