74a41412f54e4e44fadbf5426e36ece6f639f07e
spiano <lafleurpiano@gmail.com>
2019-09-23 12:22:29 -0500

    restyled the function and headings. Fixed the modal and added resume

--- script-home.js
+++ script-home.js
@@ -2,16 +2,21 @@ console.log('welcome to the rabbit hole');
 const openBtn = document.getElementById('openModal');
 const modal = document.getElementById('modal');
 const close = document.getElementById('close');
+const resumeButton = document.getElementById('resume');
 
 const openModal = () => {
     modal.style.display = 'block';
 };
-
 openBtn.addEventListener('click', openModal);
 const closeModal = () => {
-    modal.style.display = 'none'
+    modal.style.display = 'none';
 };
 close.addEventListener('click', closeModal);
+const openResume = () => {
+    document.getElementById('resume').style.display = 'block';
+    //modal.window.location.href = "WebDev-Resume.pdf";
+};
+resumeButton.addEventListener('click', openResume);
 
 const miniGamesButton = document.getElementById('mini-games');
 const practicalAppsButton = document.getElementById('practical-apps');
@@ -33,6 +38,8 @@ funStuffButton.addEventListener('click', function (e) {
     window.location.href = "http://spiano.dev/fun-stuff.html";
     }
 });
+
+
 
 
 
