e2fb2cca60913f9bcfbded7db5a5eff2731c9dff
spiano <lafleurpiano@gmail.com>
2019-11-28 14:08:33 -0600

    update complete

--- Apps/apps-script.js
+++
@@ -1,8 +0,0 @@
-console.log('welcome to the rabbit hole');
-const atmButton = document.getElementById('atm');
-
-atmButton.addEventListener('click', function (e) {
-    if (e.target) {
-        window.location.href = "http://spiano.dev/atm";
-    }
-});
--- Fun/fun-script.js
+++
@@ -1,8 +0,0 @@
-console.log('welcome to the rabbit hole');
-const pixButton = document.getElementById('pixart');
-
-pixButton.addEventListener('click', function (e) {
-    if (e.target) {
-        window.location.href = "http://spiano.dev/pixart";
-    }
-});
